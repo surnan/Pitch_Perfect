8cc5af1c1920e719d60fa05fdf723802db453960
surnan <forsuresh@yandex.com>
2018-10-03 15:35:30 -0400

    CustomNavigatoinController = initial View Controller RecordSoundsController setup as root for CustomNavigationController
    Color schemes fixed
    UINavigationpane set to .lightcontent
    UIInstructionLabel setup

--- Pitch_Perfect/Pitch_Perfect/RecordSoundsController.swift
+++ Pitch_Perfect/Pitch_Perfect/RecordSoundsController.swift
@@ -10,9 +10,50 @@ import UIKit
 
 class RecordSoundsController: UIViewController {
 
+    let instructionLabel: UILabel = {
+       let label = UILabel()
+        label.text = "Tap to start recording"
+        label.textColor = UIColor.lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let recordButton: UIButton = {
+       let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    
+    let recordIcon: UIImageView = {
+        let image = UIImageView()
+        
+        
+        return image
+    }()
+    
+    private func setupNavigationBar(){
+        navigationItem.title = "Pitch Perfect"
+    }
+    
+    private func setupUI(){
+    
+        [instructionLabel].forEach{view.addSubview($0)}
+        
+        NSLayoutConstraint.activate([
+            instructionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            instructionLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20)
+            ])
+//            instructionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+//            instructionLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.lightBlue
+        view.backgroundColor = UIColor.darkBlue
+        setupNavigationBar()
+        setupUI()
     }
 }
 
