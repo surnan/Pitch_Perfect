8f2aa2f58660d3af101642b6a9b665104d42ee00
surnan <forsuresh@yandex.com>
2018-10-03 14:28:27 -0400

    Project created and runs successfully without storyboard

---
+++ Pitch_Perfect/Pitch_Perfect/RecordSoundsController.swift
@@ -0,0 +1,18 @@
+//
+//  RecordSoundsController.swift
+//  Pitch_Perfect
+//
+//  Created by admin on 10/3/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class RecordSoundsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.lightBlue
+    }
+}
+
